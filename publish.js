4df1fbf8686f1608eaf5cb5a6173c5f4629b3240
artyr1997 <artyr.savytskyy@gmail.com>
2025-01-10 17:18:29 +0200

    Create publish.js

---
+++ publish.js
@@ -0,0 +1,7 @@
+console.log("Publish.js loaded!");
+
+// Пробуємо змінити фон сторінки
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.style.backgroundColor = "#222"; // Темний фон
+    console.log("Background color changed!");
+});
